864469be8d173c1f9478a109e349420794dd2f6e
RJ Nay <rj.nay@dhigroupinc.com>
2023-10-17 16:44:15 -0500

    profile page tweaks

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
+import { redirect, useRouter } from 'next/navigation';
 
 export default function Profile() {
   const router = useRouter();
@@ -9,17 +9,11 @@ export default function Profile() {
   if (!session) {
     router.push('/api/auth/signin');
   }
-  return (
-    <div>
-      {session ? (
-        status === 'authenticated' ? (
-          <pre>{JSON.stringify({ ...session, status }, null, 2)}</pre>
-        ) : (
-          <pre>sign in</pre>
-        )
-      ) : (
-        <div>not signed in</div>
-      )}
-    </div>
-  );
+  return session ? (
+    status === 'authenticated' ? (
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+    ) : (
+      redirect('/api/auth/signin')
+    )
+  ) : null;
 }
